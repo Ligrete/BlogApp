d4b15bedfe2c2d8026b4150b6889958610e7f7e4
Vilsur <vilsur.karimov@amberlabs.ru>
2021-07-21 04:09:25 +0300

    Feat profile page

--- apps/blog-app/src/app/app-routing.module.ts
+++ apps/blog-app/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './modules/main/main.component';
+import { ProfileComponent } from './modules/profile/profile.component';
 
 const routes: Routes = [
 
   { path: '', component:  MainComponent},
   { path: 'main', component:  MainComponent},
+  { path: 'profile', component:  ProfileComponent},
   { path: '**', redirectTo: '/'}
 ];
 
--- apps/blog-app/src/app/app.module.ts
+++ apps/blog-app/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
